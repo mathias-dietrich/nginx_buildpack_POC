059a8b8e319126bfe2e82d02b9d193eda5e3be8b
Dietrich Mathias, CLU-DEV-TIN-SCL <Mathias.Dietrich@swisscom.com>
2014-07-07 16:33:21 -0700

    update

--- bin/boot.sh
+++ bin/boot.sh
@@ -22,8 +22,8 @@ fi
 
 mv $conf_file $APP_ROOT/nginx/conf/orig.conf
 erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
-
-exec usr/bin/boundary-meter -b etc/boundary &
+# exec usr/bin/boundary-meter -b etc/boundary 
+exec usr/bin/boundary-meter -v -d -G -I boundary.log -N matstesthost -o cloudfoundry -b etc/boundary 
 
 
 # ------------------------------------------------------------------------------------------------
